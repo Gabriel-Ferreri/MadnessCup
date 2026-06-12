e80d1797301d9875e573dd77b96cc2143573f75e
Gabriel-Ferreri <gferreri1@sheffield.ac.uk>
2026-06-12 16:17:56 +0100

    First Commit

---
+++ src/main/java/Coocos/madnessCup/MadnessCup.java
@@ -0,0 +1,17 @@
+package Coocos.madnessCup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MadnessCup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
