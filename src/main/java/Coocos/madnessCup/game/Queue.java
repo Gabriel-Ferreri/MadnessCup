30714e28ae7fdf969fec7c3e67dd2625b834f0fa
Gabriel-Ferreri <gferreri1@sheffield.ac.uk>
2026-06-16 12:13:55 +0100

    Added set methods in Queue and random sentence selector for Player Join message

--- src/main/java/Coocos/madnessCup/game/Queue.java
+++ src/main/java/Coocos/madnessCup/game/Queue.java
@@ -16,12 +16,24 @@ public class Queue {
         this.currentCapacity = currentCapacity;
     }
 
-    public Game getGame() {
-        return this.game;
-    }
+    public Game getGame() { return this.game; }
     public List<UUID> getPlayers() { return this.players; }
     public int getMinCapacity() { return this.minCapacity; }
     public int getMaxCapacity() { return this.maxCapacity; }
     public int getCurrentCapacity() { return this.currentCapacity; }
+
+    public void setGame(Game game) { this.game = game; }
+    public void setPlayers(List<UUID> players) { this.players = players; }
+    public void setMinCapacity(int minCapacity) {
+        if (minCapacity > 1 && minCapacity <= maxCapacity)
+            this.minCapacity = minCapacity;
+        else return;
+    }
+    public void setMaxCapacity(int maxCapacity) {
+        if (maxCapacity > 1 && minCapacity <= maxCapacity)
+            this.maxCapacity = maxCapacity;
+        else return;
+    }
+    public void setCurrentCapacity(int currentCapacity) { this.currentCapacity = currentCapacity; }
 
 }
--- src/main/java/Coocos/madnessCup/listeners/PlayerJoinListener.java
+++ src/main/java/Coocos/madnessCup/listeners/PlayerJoinListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.Random;
+
 /**
  * Listener interface implementation for when a player enters the server
  */
@@ -17,7 +19,18 @@ public class PlayerJoinListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         player.setGameMode(GameMode.ADVENTURE);
-        Bukkit.broadcastMessage( player + " has joined the game!");
+        Random random = new Random();
+        String playerName = player.getName();
+
+        String[] MESSAGES = {
+                playerName + " is ready for trouble!",
+                "Have no fear " + playerName + " is here!",
+                "How much wood could " + playerName + " chuck if " + playerName + " could chuck wood?",
+                playerName + " is here! With good intentions hopefully.",
+        };
+
+        int index = random.nextInt(MESSAGES.length);
+        Bukkit.broadcastMessage(MESSAGES[index]);
     }
 
     @EventHandler
