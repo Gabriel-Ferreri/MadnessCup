13ef236f3a18ebe05b446f6a5535db1290ca6b9d
Gabriel-Ferreri <gferreri1@sheffield.ac.uk>
2026-06-16 12:40:08 +0100

    ReincarnationBattle class created

--- src/main/java/Coocos/madnessCup/game/Game.java
+++ src/main/java/Coocos/madnessCup/game/Game.java
@@ -1,17 +1,13 @@
 package Coocos.madnessCup.game;
 
 import Coocos.madnessCup.MadnessCup;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 import java.util.UUID;
 
 /**
  *  Game abstract class from which all the games will inherit some fundamental
- *  methods.
+ *  methods with encapsulation.
 */
 public abstract class Game {
     protected List<UUID> players;
---
+++ src/main/java/Coocos/madnessCup/game/games/ReincarnationBattle.java
@@ -0,0 +1,23 @@
+package Coocos.madnessCup.game.games;
+
+import Coocos.madnessCup.MadnessCup;
+import Coocos.madnessCup.game.Game;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ReincarnationBattle extends Game {
+    public ReincarnationBattle(MadnessCup plugin, List<UUID> players, boolean isRunning) {
+        super(plugin, players, isRunning);
+    }
+
+    @Override
+    public void startGame() {
+        this.isRunning = true;
+    }
+
+    @Override
+    public void endGame() {
+        this.isRunning = false;
+    }
+}
